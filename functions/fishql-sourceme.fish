3b54ff97b10db7ed45a2d8584df6b3f7f8ac78df
Daniel Danner <daniel@danner.de>
2023-08-07 16:45:48 +0200

    Remove obsolete file fishql-sourceme.fish

--- functions/fishql-sourceme.fish
+++
@@ -1,8 +0,0 @@
-# Source this in your config
-
-# avoid race on command exit 
-set -x _fishql_timeout 100
-set -x _fishql_session_tty (tty)
-set -x _fishql_session_euid (id -u)
-set -x _fishql_session_start (date +%s)
-
